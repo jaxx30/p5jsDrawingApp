424925ec77b4ab3afc8300941d1ffb96379d8b90
Katelyn Landaal <45806088+klandaal@users.noreply.github.com>
2021-03-05 19:33:29 -0700

    Created a main menu screen

--- sketch.js
+++ sketch.js
@@ -4,9 +4,19 @@ const DISPLAYX = 1326 / 2;
 const PICTURE_SIZE = 100;
 const PICTURE_OFFSET = 130;
 
+let overBox = false;
+let clicked = false;
+
+let followTheDots;
+let identifyTheObject;
+let colorInsideTheLines;
 let myFont;
+
 function preload() {
-  //myFont = loadFont('assets/inconsolata.otf');
+  followTheDots = loadImage("FollowTheDotedLine.png")
+  identifyTheObject = loadImage("Matching.png")
+  colorInsideTheLines = loadImage("ColorInsideTheLines.png")
+  myFont = loadFont('Inconsolata.otf');
 }
 
 function setup() {
@@ -17,19 +27,46 @@ function setup() {
 function draw() {
   background(255);
   fill(0, 102, 153);
+  let mainMenu = 0
+  
+  //main menu
   textSize(34);
   textAlign(CENTER);
-  text("Drawing App",DISPLAYX / 2 , DISPLAYY/2 -120);
- 
+  text("Main Menu", DISPLAYX / 2 , DISPLAYY/2 -120)
   
-  fill(255);
-  rect(DISPLAYX / 2 - PICTURE_SIZE / 2 - PICTURE_OFFSET, DISPLAYY / 2 - PICTURE_SIZE / 2, PICTURE_SIZE, PICTURE_SIZE);
-  rect(DISPLAYX / 2 - PICTURE_SIZE / 2, DISPLAYY / 2 - PICTURE_SIZE / 2, PICTURE_SIZE, PICTURE_SIZE);
-  rect(DISPLAYX / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, DISPLAYY / 2 - PICTURE_SIZE / 2, PICTURE_SIZE, PICTURE_SIZE);
+  textSize(15);
+  text("Follow the Dots", DISPLAYX / 3 - PICTURE_SIZE, DISPLAYY / 3 + PICTURE_SIZE / 2);
+  image(followTheDots, DISPLAYX / 3 - 190, DISPLAYY / 3 + PICTURE_SIZE / 2 + 5, 175, 175);
+  
+  text("Identify the Object", DISPLAYX / 2 - 5, DISPLAYY / 3 + PICTURE_SIZE / 2);
+  image(identifyTheObject, DISPLAYX / 2 - 91, DISPLAYY / 3 + PICTURE_SIZE / 2 + 5, 175, 175);
+  
+  text("Color Inside the Lines", DISPLAYX / 2 + PICTURE_OFFSET * 1.5, DISPLAYY / 3 + PICTURE_SIZE / 2);
+  image(colorInsideTheLines, DISPLAYX / 2 + PICTURE_OFFSET * 1.5 - 87, DISPLAYY / 3 + PICTURE_SIZE / 2 + 5, 175, 175);
+  
+  if(mouseX > DISPLAYX / 3 - 190 && mouseX < DISPLAYX / 3 - 190 + 175 && mouseY > DISPLAYY / 3 + PICTURE_SIZE / 2 + 5 && mouseY < DISPLAYY / 3 + PICTURE_SIZE / 2 + 5 +175)
+  {
+    overBox = true
+    if(clicked == true){
+      clear();
+      textSize(34);
+      textAlign(CENTER);
+      text("Drawing App",DISPLAYX / 2 , DISPLAYY/2 -120);
 
-  rect(DISPLAYX / 2 - PICTURE_SIZE / 2 - PICTURE_OFFSET, DISPLAYY / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, PICTURE_SIZE, PICTURE_SIZE);
-  rect(DISPLAYX / 2 - PICTURE_SIZE / 2, DISPLAYY / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, PICTURE_SIZE, PICTURE_SIZE);
-  rect(DISPLAYX / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, DISPLAYY / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, PICTURE_SIZE, PICTURE_SIZE);
 
+      fill(255);
+      rect(DISPLAYX / 2 - PICTURE_SIZE / 2 - PICTURE_OFFSET, DISPLAYY / 2 - PICTURE_SIZE / 2, PICTURE_SIZE, PICTURE_SIZE);
+      rect(DISPLAYX / 2 - PICTURE_SIZE / 2, DISPLAYY / 2 - PICTURE_SIZE / 2, PICTURE_SIZE, PICTURE_SIZE);
+      rect(DISPLAYX / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, DISPLAYY / 2 - PICTURE_SIZE / 2, PICTURE_SIZE, PICTURE_SIZE);
 
+      rect(DISPLAYX / 2 - PICTURE_SIZE / 2 - PICTURE_OFFSET, DISPLAYY / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, PICTURE_SIZE, PICTURE_SIZE);
+      rect(DISPLAYX / 2 - PICTURE_SIZE / 2, DISPLAYY / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, PICTURE_SIZE, PICTURE_SIZE);
+      rect(DISPLAYX / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, DISPLAYY / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, PICTURE_SIZE, PICTURE_SIZE);
+    }
+  }
+
+}
+
+function mousePressed(){
+    clicked = true;
 }
