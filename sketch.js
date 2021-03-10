ddcfa5320185c9d8f2ec671e644c877fc1810733
jaxx30 <42377090+jaxx30@users.noreply.github.com>
2021-03-09 21:38:31 -0700

    Fixed bug and added font

--- sketch.js
+++ sketch.js
@@ -2,7 +2,11 @@ const DISPLAYY = 1024 / 2;
 const DISPLAYX = 1326 / 2;
 
 const PICTURE_SIZE = 100;
+const PICTURE_SIZEX = 139;
+const PICTURE_SIZEY= 200;
+
 const PICTURE_OFFSET = 130;
+const GALLERY_OFFSET =  160;
 
 let overBox = false;
 let clicked = false;
@@ -11,17 +15,22 @@ let followTheDots;
 let identifyTheObject;
 let colorInsideTheLines;
 let myFont;
+let img1;
+
 
 function preload() {
   followTheDots = loadImage("FollowTheDotedLine.png")
   identifyTheObject = loadImage("Matching.png")
   colorInsideTheLines = loadImage("ColorInsideTheLines.png")
-  myFont = loadFont('Inconsolata.otf');
+ // img1 = loadImage('./Images/how-to-draw-an-elephant.jpg');
+  myFont = loadFont('./Fonts/PatrickHand-Regular.ttf');
 }
+
 
 function setup() {
   createCanvas(DISPLAYX, DISPLAYY);
-   
+    textFont(myFont);
+
 }
 
 function draw() {
@@ -30,6 +39,7 @@ function draw() {
   let mainMenu = 0
   
   //main menu
+
   textSize(34);
   textAlign(CENTER);
   text("Main Menu", DISPLAYX / 2 , DISPLAYY/2 -120)
@@ -55,13 +65,12 @@ function draw() {
 
 
       fill(255);
-      rect(DISPLAYX / 2 - PICTURE_SIZE / 2 - PICTURE_OFFSET, DISPLAYY / 2 - PICTURE_SIZE / 2, PICTURE_SIZE, PICTURE_SIZE);
-      rect(DISPLAYX / 2 - PICTURE_SIZE / 2, DISPLAYY / 2 - PICTURE_SIZE / 2, PICTURE_SIZE, PICTURE_SIZE);
-      rect(DISPLAYX / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, DISPLAYY / 2 - PICTURE_SIZE / 2, PICTURE_SIZE, PICTURE_SIZE);
+    
+  rect(DISPLAYX / 2 - PICTURE_SIZEX / 2 - GALLERY_OFFSET, DISPLAYY / 2 - PICTURE_SIZEX / 2, PICTURE_SIZEX, PICTURE_SIZEY, 15);
+   // image(img1,DISPLAYX / 2 - PICTURE_SIZEX / 2 - PICTURE_OFFSET + 5, DISPLAYY / 2 - PICTURE_SIZEX / 4 , PICTURE_SIZEX -10 , PICTURE_SIZEY -80);
+  rect(DISPLAYX / 2 - PICTURE_SIZEX / 2, DISPLAYY / 2 - PICTURE_SIZEX / 2, PICTURE_SIZEX, PICTURE_SIZEY, 15);
+  rect(DISPLAYX / 2 - PICTURE_SIZEX / 2 +  GALLERY_OFFSET, DISPLAYY / 2 - PICTURE_SIZEX / 2, PICTURE_SIZEX, PICTURE_SIZEY, 15);
 
-      rect(DISPLAYX / 2 - PICTURE_SIZE / 2 - PICTURE_OFFSET, DISPLAYY / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, PICTURE_SIZE, PICTURE_SIZE);
-      rect(DISPLAYX / 2 - PICTURE_SIZE / 2, DISPLAYY / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, PICTURE_SIZE, PICTURE_SIZE);
-      rect(DISPLAYX / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, DISPLAYY / 2 - PICTURE_SIZE / 2 + PICTURE_OFFSET, PICTURE_SIZE, PICTURE_SIZE);
     }
   }
 
@@ -69,4 +78,8 @@ function draw() {
 
 function mousePressed(){
     clicked = true;
+}
+
+function mouseReleased(){
+  clicked = false;
 }
